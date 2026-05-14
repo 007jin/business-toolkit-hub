eee2d3f707409c49df772f5db195695b013e9369
007jin <007jin.ai@proton.me>
2026-05-15 01:18:21 +0800

    Fixed next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-@type {import('next').NextConfig}
+import type { NextConfig } from 'next';
 
-const nextConfig = {};
+const nextConfig: NextConfig = {
+  // your Next.js config options here
+};
 
 export default nextConfig;
